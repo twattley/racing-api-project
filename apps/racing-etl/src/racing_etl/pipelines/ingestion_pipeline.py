65cf36a6a8a9e6c158dfa3616c7a6a2c4762d304
twattley <wattley83@gmail.com>
2025-09-11 10:58:00 +0100

    wip

--- apps/racing-etl/src/racing_etl/pipelines/ingestion_pipeline.py
+++ apps/racing-etl/src/racing_etl/pipelines/ingestion_pipeline.py
@@ -12,35 +12,35 @@ from ..raw.timeform.ingestor import TFIngestor
 def run_ingestion_pipeline(
     storage_client: IStorageClient
 ):
-    # chat_model = ChatModels(model_name="google")
-    # rp_driver = WebDriver(config, headless_mode=True, website="racingpost")
+    chat_model = ChatModels(model_name="google")
+    rp_driver = WebDriver(config, headless_mode=False, website="racingpost")
 
-    # rp_ingestor = RPIngestor(
-    #     config=config,
-    #     storage_client=storage_client,
-    #     chat_model=chat_model,
-    #     driver=rp_driver,
-    # )
+    rp_ingestor = RPIngestor(
+        config=config,
+        storage_client=storage_client,
+        chat_model=chat_model,
+        driver=rp_driver,
+    )
 
-    # rp_ingestor.ingest_results_links()
-    # rp_ingestor.ingest_todays_links()
-    # rp_ingestor.ingest_todays_data()
-    # rp_ingestor.ingest_results_data()
-    # rp_ingestor.ingest_results_data_world()
+    rp_ingestor.ingest_results_links()
+    rp_ingestor.ingest_todays_links()
+    rp_ingestor.ingest_todays_data()
+    rp_ingestor.ingest_results_data()
+    rp_ingestor.ingest_results_data_world()
 
     tf_driver = WebDriver(config, headless_mode=False, website="timeform")
     tf_ingestor = TFIngestor(
         config=config, storage_client=storage_client, driver=tf_driver
     )
 
-    # tf_ingestor.ingest_results_links()
-    # tf_ingestor.ingest_todays_links()
-    # tf_ingestor.ingest_todays_data()
+    tf_ingestor.ingest_results_links()
+    tf_ingestor.ingest_todays_links()
+    tf_ingestor.ingest_todays_data()
     tf_ingestor.ingest_results_data()
-    # tf_ingestor.ingest_results_data_world()
+    tf_ingestor.ingest_results_data_world()
 
-    # betfair_client = get_betfair_client()
-    # bf_ingestor = BFIngestor(
-    #     config=config, storage_client=storage_client, betfair_client=betfair_client
-    # )
-    # bf_ingestor.ingest_todays_data()
+    betfair_client = get_betfair_client()
+    bf_ingestor = BFIngestor(
+        config=config, storage_client=storage_client, betfair_client=betfair_client
+    )
+    bf_ingestor.ingest_todays_data()
